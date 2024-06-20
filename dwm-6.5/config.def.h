8f27502cafa7fd5a1d12b491006f09aac8bb8350
Colby <colby.braden@gmail.com>
2024-06-20 12:19:53 -0500

    updated dwm build

--- dwm-6.5/config.def.h
+++ dwm-6.5/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -20,7 +20,7 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray2, col_gray3,  col_gray3  },
 	[SchemeTitle]  = { col_gray4, col_gray1,  col_cyan  },
 };
 
@@ -80,7 +80,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 static const char *rofi[]  = { "rofi", "-show", "drun", "-show-icons", "-run-shell-command", NULL };
 static const char *fileman[]  = { "pcmanfm", NULL };
-static const char *browser[]  = { "flatpak", "run", "org.mozilla.firefox", NULL };
+static const char *browser[]  = { "brave-browser", NULL };
 static const char *game[]  = { "flatpak", "run", "net.lutris.Lutris", NULL };
 static const char *screenshot[] = { "scrot", NULL };
 
